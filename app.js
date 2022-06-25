3514cf5623f4fcadfae8ca5636f7825b3db36742
norbert <norberto.a.vega@gmail.com>
2022-06-24 22:58:37 -0300

    v0.0.1

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+const PORT = process.env.PORT || 8000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor express excuchando en el puerto ${PORT}`);
+});
